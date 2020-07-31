2b5382cc2bcf7fd97a94bdb90493410b264184a7
Andrew Potter <apottere@gmail.com>
2020-07-30 23:11:45 -0400

    Updates

---
+++ precache-manifest.3b6768c92e5414ae57758f1fad9516de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e96fd57b0ed2d2de5f4cdcdba28c42d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "592626a7d2eecf42c3b2",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82c0784530db85c7bed7",
+    "url": "/static/js/2.8e1a7ff6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8e1a7ff6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592626a7d2eecf42c3b2",
+    "url": "/static/js/main.09c7ba2f.chunk.js"
+  },
+  {
+    "revision": "a84207d9ba2c991a30d7",
+    "url": "/static/js/runtime-main.01ea0e31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
